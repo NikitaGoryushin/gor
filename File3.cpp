944e6c93c8c663188dabae61a59c14d8615eeebe
NikitaGoryushin <55922018+NikitaGoryushin@users.noreply.github.com>
2019-09-28 23:10:06 +0300

    Add files via upload

---
+++ File3.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <stdio.h>
+#include <string.h>
+
+using namespace std;
+
+int main() {
+	char str[80];
+	cout << "Enter string:\n";
+	cin.getline(str, 80);
+	int len = strlen(str);
+	int i, count = 1, line = 1;
+
+	for (i = 0; i < len; i++) {
+		if (str[i] == ' ') {
+			count++;
+		}
+		else if (str[i] == '.') {
+			count = 0;
+			line++;
+		}
+	}
+	cout << "count =" << count << " v " << line << " predlojenie";
+		 system ("pause");
+	return 0;
+
+}
+//	int main() {
+//		setlocale(LC_ALL, "Russian");
+//		char str[80];
+//		cout << "Enter string:\n";
+//		cin.getline(str, 80);
+//		int len = strlen(str);
+//		int i, count = 1, line = 1;
+//
+//		for (i = 0; i < len; i++) {
+//			if (str[i] == ' ') {
+//				atoi(str[i]) = atoi(str[i]) + 20;
+//				count++;
+//			}
+//			else if (str[i] == '.') {
+//				count = 0;
+//				line++;
+//			}
+//		}
+//		cout << "count =" << count << " v " << line << " predlojenie";
+//
+//		return 0;
+//	}
+//}
