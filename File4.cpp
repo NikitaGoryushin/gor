d434fe86e1635b5a864810d83b68fd7a549ea11f
NikitaGoryushin <55922018+NikitaGoryushin@users.noreply.github.com>
2019-09-28 23:41:02 +0300

    Add files via upload

---
+++ File4.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <stdio.h>
+#include <string.h>
+
+using namespace std;
+
+w = 'слово'
+text = input(str(a))
+
+c = text.count(w)
+
+while c > 0:
+	print(w)
+    c -= 1
+f1 = open('C:\Tranz2014.txt','r',encoding="utf-8")
+text1 = input(str(a));
+text1.lower()
+f1.close()
+i=0
+for i in range(len(w)):
+    print((w[i])+"-"+str(text.lower().count(w[i])))
+f2 = open('C:\Tranz2015.txt','r',encoding="utf-8")
+text2 = f2.read()
+text2.lower()
+f2.close()
+i=0
+for i in range(len(w)):
+    print((w[i])+"-"+str(text.lower().count(w[i])))
+f3 = open('C:\Tranz2016.txt','r',encoding="utf-8")
+text3 = f3.read()
+text3.lower()
+f3.close()
+i=0
+for i in range(len(w)):
+    print((w[i])+"-"+str(text.lower().count(w[i])))
+f4 = open('C:\Tranz2017.txt','r',encoding="utf-8")
+text4 = f4.read()
+text4.lower()
+f4.close()
+i=0
+for i in range(len(w)):
+    print((w[i])+"-"+str(text.lower().count(w[i])))
+f5 = open('C:\Tranz2018.txt','r',encoding="utf-8")
+text5 = f5.read()
+text5.lower()
+f5.close()
+i=0
+for i in range(len(w)):
+    print((w[i])+"-"+str(text.lower().count(w[i])))
+f6 = open('C:\Tranz2019.txt','r',encoding="utf-8")
+text6 = f6.read()
+text6.lower()
+f6.close()
+i=0
+for i in range(len(w)):
+    print((w[i])+"-"+str(text.count(w[i])))
+
+
